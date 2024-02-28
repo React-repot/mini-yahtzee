97ba9d962cf8f6d3e6e9c0bdff5e92dd12c492f0
Aku Nikula <aku.nikula@gmail.com>
2024-02-28 22:35:13 +0200

    asd

--- components/Home.js
+++ components/Home.js
@@ -5,32 +5,33 @@ import { TextInput } from 'react-native-paper'
 
 export default function Home({navigation}) {
 
-const [playerName, setPlayerName] = useState('')
-const [hasPlayerName, setHasPlayerName] = useState(false)
+  const [playerName, setPlayerName] = useState('')
+  const [hasPlayerName, setHasPlayerName] = useState(false)
 
-const handlePlayerName = () => {
+  const handlePlayerName = () => {
 
-  return (
-    
-    { !hasPlayerName ?
-      <>
-        <Text>For scörrebröd enter name</Text>
-        <TextInput onChangeText={setPlayerName} autoFocus={true} />
-          <Pressable onPress={handlePlayerName(playerName)}>
-            <Text>OK</Text>
+    return (
+      
+      { !hasPlayerName ?
+        <>
+          <Text>For scörrebröd enter name</Text>
+          <TextInput onChangeText={setPlayerName} autoFocus={true} />
+            <Pressable onPress={handlePlayerName(playerName)}>
+              <Text>OK</Text>
+            </Pressable>
+        </>
+        :
+        <View>
+          <Text>Rules</Text>
+          <Text multiline="true">
+            Rule goes here ebin lmao hahahahahahhahahahahahahhahahahahahhahahahaha lorem ipsum opossum 
+          </Text>
+          <Text>Good luck {playerName}</Text>
+          <Pressable onPress={() => navigation.navigate("Gameboard", {player: playerName})}>
+            <Text>Spelar ett spel</Text>
           </Pressable>
-      </>
-      :
-      <View>
-        <Text>Rules</Text>
-        <Text multiline="true">
-          Rule goes here ebin lmao hahahahahahhahahahahahahhahahahahahhahahahaha lorem ipsum opossum 
-        </Text>
-        <Text>Good luck {playerName}</Text>
-        <Pressable onPress={() => navigation.navigate("Gameboard", {player: playerName})}>
-          <Text>Spelar ett spel</Text>
-        </Pressable>
-      </View>
-    }
-  )
+        </View>
+      }
+    )
+  }
 }
