3997eecef3a6a011bf9cfd32a0337ec894de59f4
Aku Nikula <aku.nikula@gmail.com>
2024-02-29 11:07:51 +0200

    29.02 Update

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -2,14 +2,24 @@ import { View, Text, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-native-flex-grid'
 import { MaterialCommunityIcons } from "@expo/vector-icons/MaterialCommunityIcons"
-// IMPORT CONSTANTS, HEADER, FOOTER
+import {
+  NBR_OF_DICE,
+  NBR_OF_THROWS,
+  MIN_SPOT,
+  MAX_SPOT,
+  BONUS_LIMIT,
+  BONUS_POINT
+} from '../constants/Game'
+import Header from './Header'
+import Footer from './Footer'
+
 
 let board = []
 
 export default function Gameboard({ navigation, route }) {
 
   const [playerName, setPlayerName] = useState('')
-  const [throwsLeft, setThrowsLeft] = useState(/* HEITTOVAKIO */)
+  const [throwsLeft, setThrowsLeft] = useState(NBR_OF_THROWS)
   const [status, setStatus] = useState("Throw dice")
   const [gameEndStatus, setGameEndStatus] = useState(false)
 
@@ -39,17 +49,59 @@ export default function Gameboard({ navigation, route }) {
                 name={board[i]}
                 key={'diceRow' + i}
                 size={50}
-                /* color={getDiceColor(i)} */ >
+                color={getDiceColor(i)} >
                 </MaterialCommunityIcons>
             </Pressable>
           </Col>
         )
     }
+
+  
+  const pointsRow = []
+  for (let spot = 0; spot < MAX_SPOT; spot++) {
+    pointsRow.push(
+      <Col key={"pointsRow" + spot}>
+        <Text key={"pointsRow" + spot}>
+          {getSpotTotal(diceButton)}
+        </Text>
+      </Col>
+    )
+    
+  }
+
+  const pointsToSelectRow = []
+  for (let diceButton = 0; diceButton < MAX_SPOT; diceButton++) {
+    pointsToSelectRow.push(
+      <Col key={"buttonsRow" + diceButton}>
+        <Pressable key={"buttonsRow" + diceButton} onPress={() => selectDicePoints(diceButton)}>
+          <MaterialCommunityIcons
+            name={"numeric-" + (diceButton+1) + "-circle"}
+            key={"buttonsRow" + diceButton}
+            size={35}
+            color={getDicePointsColor(diceButton)}
+          >
+
+          </MaterialCommunityIcons>
+        </Pressable>
+      </Col>
+    )
+  }
   
   function getDiceColor(i) {
         return diceSelected[i] ? 'black' : 'steelblue'
     }
+
+  function getDicePointsColor(i) {
+    if (selectDicePoints[i] && !gameEndStatus) {
+      return "black"
+    } else {
+      return "steelblue"
+    }
+  }
   
+  function getSpotTotal(i) {
+    return dicePointsTotal[i]
+  }
 
   const selectDice = (i) => {
       let dice = [...diceSelected]
@@ -67,18 +119,39 @@ export default function Gameboard({ navigation, route }) {
       setThrowsLeft(throwsLeft-1)
   }
 
+  const selectDicePoints= (i) => {
+    //Early version
+    let selectDice = [...diceSelected]
+    let selectedPoints = [...selectedDicePoints]
+    let points = [...dicePointsTotal]
+    selectedDicePoints[i] = true
+    let nbrOfDice = diceSpots.reduce((total, x) => (x === (i + 1) ? total + 1 : total), 0)
+    points[i] = nbrOfDice * (i + 1)
+    setDicePointsTotal(points)
+    setSelectedDicePoints(selectedPoints)
+    return points[i]
+  }
+
   return (
     <>
+      <Header />
       <View>
         <Text>Görrebröd</Text>
         <Container fluid>
           <Row>{diceRow}</Row>
+        </Container>
+        <Container fluid>
+          <Row>{pointsRow}</Row>
+        </Container>
+        <Container fluid>
+          <Row>{pointsToSelectRow}</Row>
         </Container>
         <Pressable onPress={() => throwDice()}>
           THROW
         </Pressable>
         <Text>Player: {playerName}</Text>
       </View>
+      <Footer />
     </>
   )
 }
--- components/Home.js
+++ components/Home.js
@@ -1,37 +1,53 @@
 import { View, Text, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import { TextInput } from 'react-native-paper'
-
+import {
+  NBR_OF_DICE,
+  NBR_OF_THROWS,
+  MIN_SPOT,
+  MAX_SPOT,
+  BONUS_LIMIT,
+  BONUS_POINT
+} from '../constants/Game'
 
 export default function Home({navigation}) {
 
   const [playerName, setPlayerName] = useState('')
   const [hasPlayerName, setHasPlayerName] = useState(false)
 
-  const handlePlayerName = () => {
-
-    return (
-      
-      { !hasPlayerName ?
-        <>
-          <Text>For scörrebröd enter name</Text>
-          <TextInput onChangeText={setPlayerName} autoFocus={true} />
-            <Pressable onPress={handlePlayerName(playerName)}>
-              <Text>OK</Text>
-            </Pressable>
-        </>
-        :
-        <View>
-          <Text>Rules</Text>
-          <Text multiline="true">
-            Rule goes here ebin lmao hahahahahahhahahahahahahhahahahahahhahahahaha lorem ipsum opossum 
-          </Text>
-          <Text>Good luck {playerName}</Text>
-          <Pressable onPress={() => navigation.navigate("Gameboard", {player: playerName})}>
-            <Text>Spelar ett spel</Text>
-          </Pressable>
-        </View>
-      }
-    )
+  const handlePlayerName = (value) => {
+    if (value.trim().length > 0) {
+        setHasPlayerName(true)
+        Keyboard.dismiss()
+    }
   }
+
+  return (
+    <>
+      <Header />
+      <View>
+        { !hasPlayerName ?
+          <>
+            <Text>For scörrebröd enter name</Text>
+            <TextInput onChangeText={setPlayerName} autoFocus={true} />
+              <Pressable onPress={handlePlayerName(playerName)}>
+                <Text>OK</Text>
+              </Pressable>
+          </>
+          :
+          <View>
+            <Text>Rules</Text>
+            <Text multiline="true">
+              Rule goes here ebin lmao hahahahahahhahahahahahahhahahahahahhahahahaha lorem ipsum opossum 
+            </Text>
+            <Text>Good luck {playerName}</Text>
+            <Pressable onPress={() => navigation.navigate("Gameboard", {player: playerName})}>
+              <Text>Spelar ett spel</Text>
+            </Pressable>
+          </View>
+        }
+      </View>
+      <Footer />
+    </>
+  )
 }
